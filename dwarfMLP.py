c719961191c07e8a0c14add119a9020bc7a3d7cf
sfgeekgit <sfgeek@gmail.com>
2024-02-06 20:35:53 +0000

    much better model trainer, but still needs better

--- dwarfMLP.py
+++ dwarfMLP.py
@@ -12,13 +12,20 @@ from sklearn.preprocessing import StandardScaler
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = "cpu"
 print(f"{device=}")
 
-seed_value = 777
+# todo... currently drop FortSurvived from the data, maybe put that back in
+# to test: how does this perform on only forts that survived?  If it's near perfect then it's not prediting who survives.
+
+
+seed_value = 777   # rep-digits are fun and also easy to type
 
 # Set seeds to ensure reproducibility
 torch.manual_seed(seed_value)
 
+
+## Data loader is bottleneck on GPU, makes cuda basically useless
 
 
 # Load the dataset
@@ -60,8 +67,11 @@ y_test_tensor = torch.tensor(y_test.values, dtype=torch.float).view(-1, 1).to(de
 train_dataset = TensorDataset(X_train_tensor, y_train_tensor.view(-1, 1))
 test_dataset = TensorDataset(X_test_tensor, y_test_tensor.view(-1, 1))
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+## To make use of GPU, need a big batch!  (Or do we? Maybe that was the dataloader?? experiment with batch size?
+
+train_loader = DataLoader(train_dataset, batch_size=1064, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1064, shuffle=False)
 
 # Define the MLP model
 class MLP(nn.Module):
@@ -69,8 +79,10 @@ class MLP(nn.Module):
         # input size is 19 for this dataset
         #print("Create Model. input_size: ", input_size)
         super(MLP, self).__init__()
-        hidden = 32
-        dropout = 0.2
+        
+        # trying a wiiide hidden, just trying what works?
+        hidden = 320
+        dropout = 0
         self.layers = nn.Sequential(
         nn.Linear(input_size, hidden),            
         nn.BatchNorm1d(hidden),
@@ -85,41 +97,68 @@ class MLP(nn.Module):
         nn.BatchNorm1d(hidden),
         nn.ReLU(),
         nn.Dropout(dropout),
+        nn.Linear(hidden, hidden),
+        nn.BatchNorm1d(hidden),
+        nn.ReLU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden, hidden),
+        nn.BatchNorm1d(hidden),
+        nn.ReLU(),
+        nn.Dropout(dropout),
         nn.Linear(hidden, 1)
         )
+        print (self.layers)
     
     def forward(self, x):
         return self.layers(x)
 
 # Initialize the model, loss function, and optimizer
-##model = MLP(X_train_tensor.shape[1])
+#model = MLP(X_train_tensor.shape[1])
 model = MLP(X_train_tensor.shape[1]).to(device)
 
 
 criterion = nn.MSELoss()  # Mean Squared Error Loss for regression tasks
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+
+# Just load the whole thing every epoch. the dataloader was tight bottleneck for GPU
+
+targets = y_train_tensor # [:2]  # test a tiny dataset
+data = X_train_tensor # [:2]    # test a tiny dataset
+
+
+
 # Training the model
-epochs = 1000
+epochs = 10000
 for epoch in range(epochs):
+
     model.train()
 
-    for data, targets in train_loader:
-
-        data, targets = data.to(device), targets.to(device) 
-
-
-        # Zero the gradients
-        optimizer.zero_grad()
-        
-        # Forward pass
-        outputs = model(data)
-        loss = criterion(outputs, targets)
-        
-        # Backward pass and optimize
-        loss.backward()
-        optimizer.step()
+    #data = X_train_tensor
+    #targets = y_train_tensor
+    #for data, targets in train_loader:
     
-    print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+    #data, targets = data.to(device), targets.to(device) 
 
+    # Zero the gradients
+    optimizer.zero_grad()
+        
+    # Forward pass
+    outputs = model(data)
+    loss = criterion(outputs, targets)
+
+
+    # Backward pass and optimize
+    loss.backward()
+    optimizer.step()
+
+    if epoch % 10 == 0:    
+        print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+        #print(f"{outputs.shape=}")
+        #print(f"{targets.shape=}")
+
+
+
+# Save the model...
+        
 # Add code here to evaluate the model on the test set and make predictions
